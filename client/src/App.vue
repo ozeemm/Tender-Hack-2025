80527ad102c3fb3e1dbe5f0781d3e1755f2d9cbb
ozeemm <77226278+ozeemm@users.noreply.github.com>
2025-03-02 04:07:36 +0800

    Changed TableCreator block style

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,87 +1,88 @@
 <script setup>
-    import { ref, useTemplateRef } from 'vue'
+import { ref, useTemplateRef } from 'vue'
 
-    import Chart from 'chart.js/auto';
+import Chart from 'chart.js/auto';
 
-    import MetricItem from "./components/MetricItem.vue"
-    import TableBlock from "./components/TableBlock.vue"
-    import TableCreator from "./components/TableCreator.vue"
+import MetricItem from "./components/MetricItem.vue"
+import TableBlock from "./components/TableBlock.vue"
+import TableCreator from "./components/TableCreator.vue"
 
-    const testChart = useTemplateRef('testChart')
-    const innInput = useTemplateRef('innInput')
-    const metricsParamsCollapseButton = useTemplateRef('metricsParamsCollapseButton')
+const testChart = useTemplateRef('testChart')
+const innInput = useTemplateRef('innInput')
+const metricsParamsCollapseButton = useTemplateRef('metricsParamsCollapseButton')
 
-    const metrics = ref([
-        { metric: "Метрика", value: "10000000 млн." },
-        { metric: "Метрика", value: "32.4%" },
-        { metric: "Метрика", value: "250" }
-    ])
-    
-    const INN = ref("123")
-    const metricsParams = ref({ "param": "param1" })
+const metrics = ref([
+    { metric: "Метрика", value: "10000000 млн." },
+    { metric: "Метрика", value: "32.4%" },
+    { metric: "Метрика", value: "250" }
+])
 
-    function onInnSubmit(){
-        INN.value = innInput.value
-    }
+const INN = ref("Меня забыли")
+const metricsParams = ref({ "param": "param1" })
 
-    function showChart(){
-        const data = [
-            { year: 2010, count: 10 },
-            { year: 2011, count: 20 },
-            { year: 2012, count: 15 },
-            { year: 2013, count: 25 },
-            { year: 2014, count: 22 },
-            { year: 2015, count: 30 },
-            { year: 2016, count: 28 },
-        ];
+function onInnSubmit() {
+    INN.value = innInput.value
+}
 
-        testChart.value.hidden = false
+function showChart() {
+    const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+    ];
 
-        new Chart(testChart.value, {
-            type: 'bar',
-            data: {
-                labels: data.map(row => row.year),
-                datasets: [
-                    {
-                        label: 'Acquisitions by year',
-                        data: data.map(row => row.count)
-                    }
-                ]
-            }
-        })
-    }
+    testChart.value.hidden = false
 
-    const blocks = ref([])
-    const isBlockCreating = ref(false)
-    const creatingBlock = ref()
-
-    function onAddtableClick(){
-        isBlockCreating.value = true
-        creatingBlock.value = TableCreator
-    }
-
-    function onSubmitBlockCreating(params){
-        if(params.type == 'table'){
-            blocks.value.push({ component: TableBlock, params: params })
+    new Chart(testChart.value, {
+        type: 'bar',
+        data: {
+            labels: data.map(row => row.year),
+            datasets: [
+                {
+                    label: 'Acquisitions by year',
+                    data: data.map(row => row.count)
+                }
+            ]
         }
+    })
+}
 
-        onCancelBlockCreating()
+const blocks = ref([])
+const isBlockCreating = ref(false)
+const creatingBlock = ref()
+
+function onAddTableClick() {
+    isBlockCreating.value = true
+    creatingBlock.value = TableCreator
+}
+
+function onSubmitBlockCreating(params) {
+    if (params.type == 'table') {
+        blocks.value.push({ component: TableBlock, params: params })
     }
 
-    function onBlockDeleteClick(index){
-        blocks.value.splice(index, 1)
-    }
+    onCancelBlockCreating()
+}
 
-    function onCancelBlockCreating(){
-        isBlockCreating.value = false
-        creatingBlock.value = null
-    }
+function onBlockDeleteClick(index) {
+    blocks.value.splice(index, 1)
+}
+
+function onCancelBlockCreating() {
+    isBlockCreating.value = false
+    creatingBlock.value = null
+}
+
 </script>
 
 <template>
     <div class="container mt-2 mb-2" v-if="INN != null">
         <div>
-            <h5>Не забудь стартовую страничку с ИНН</h5>
+            ИНН: {{ INN }}<br>
             metricsParams: {{ metricsParams }}<br>
             isBlockCreating: {{ isBlockCreating }}
         </div>
@@ -89,34 +90,50 @@
         <div class="row">
             <!-- Блоки -->
             <div class="col-10">
-                <!--Отображение блоков-->
-                <div v-for="(block, index) in blocks">
-                    <component :is="block.component" :params="block.params" :id="index" @onDeleteClick="onBlockDeleteClick"/>
+                <!-- Отображение блоков -->
+                <div v-for="(block, index) in blocks" class="mb-2 mt-2">
+                    <component :is="block.component" :params="block.params" :id="index"
+                        @onDeleteClick="onBlockDeleteClick"/>
                 </div>
-                
-                <!--Отображение -->
+
+                <!-- Отображение блока создания -->
                 <div v-if="isBlockCreating">
-                    <component :is="creatingBlock" @onSubmitClick="onSubmitBlockCreating" @onCancelClick="onCancelBlockCreating"/>
+                    <component :is="creatingBlock" 
+                        @onSubmitClick="onSubmitBlockCreating"
+                        @onCancelClick="onCancelBlockCreating"/>
                 </div>
 
                 <div class="border mt-2">
-                    <canvas class="m-2 p-4 border" ref="testChart" hidden="true"/>
+                    <canvas class="m-2 p-4 border" ref="testChart" hidden="true" />
                 </div>
 
-                <button v-if="!isBlockCreating" @click="onAddtableClick">Добавить таблицу</button>
+                <!--Кнопка "Добавить"-->
+                <div v-if="!isBlockCreating" class="d-flex justify-content-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown"aria-expanded="false">
+                            Добавить
+                        </button>
 
-
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#" @click="onAddTableClick">Таблица</a></li>
+                            <li><a class="dropdown-item" href="#" @click="console.log('Создай что-то ещё')">Что-то ещё</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <!-- Метрики -->
             <div class="col-2 border">
                 <div class="text-end" data-bs-toggle="collapse" href="#collapseMetricsParams">O</div>
                 <div class="collapse border p-2" id="collapseMetricsParams" ref="metricsParamsCollapseButton">
-                        <label><input type="radio" name="metricsRadioButton" value="param1" v-model="metricsParams.param"> Параметр 1</label>
-                        <label><input type="radio" name="metricsRadioButton" value="param2" v-model="metricsParams.param"> Параметр 2</label>
-                        <label><input type="radio" name="metricsRadioButton" value="param3" v-model="metricsParams.param"> Параметр 3</label>
+                    <label><input type="radio" name="metricsRadioButton" value="param1" v-model="metricsParams.param">
+                        Параметр 1</label>
+                    <label><input type="radio" name="metricsRadioButton" value="param2" v-model="metricsParams.param">
+                        Параметр 2</label>
+                    <label><input type="radio" name="metricsRadioButton" value="param3" v-model="metricsParams.param">
+                        Параметр 3</label>
                 </div>
-                <metric-item v-for="metric in metrics" :metric="metric.metric" :value="metric.value"/>
+                <metric-item v-for="metric in metrics" :metric="metric.metric" :value="metric.value" />
             </div>
         </div>
     </div>
@@ -129,7 +146,7 @@
 </template>
 
 <style scoped>
-    body{
-        color: #1A1A1A;
-    }
+body {
+    color: #1A1A1A;
+}
 </style>
