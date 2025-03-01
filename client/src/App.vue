00f57fc2a49dce3c06818c188f73e6521cf914f5
ozeemm <77226278+ozeemm@users.noreply.github.com>
2025-03-02 00:51:05 +0800

    Storing data from filters

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,10 @@
     import MetricItem from "./components/MetricItem.vue"
     import MyTable from "./components/MyTable.vue"
 
+    const testChart = useTemplateRef('testChart')
+    const innInput = useTemplateRef('innInput')
+    const metricsParamsCollapseButton = useTemplateRef('metricsParamsCollapseButton')
+
     const metrics = ref([
         { metric: "Метрика", value: "10000000 млн." },
         { metric: "Метрика", value: "32.4%" },
@@ -19,11 +23,10 @@
             ["01.13.13.01.01", "РАСХОДНЫЕ МАТЕРИАЛЫ И КОМПЛЕКТУЮЩИЕ ДЛЯ ЛАЗЕРНЫХ ПРИНТЕРОВ И МФУ", "7721663977", "2022-12-02 09:18:07.577", "2022-12-02 15:17:08.287" ]
         ]
     })
-
-    const testChart = useTemplateRef('testChart')
-    const innInput = useTemplateRef('innInput')
     
     const INN = ref()
+    const filters = ref({})
+    const metricsParams = ref({ "param": "param1" })
 
     function onInnSubmit(){
         INN.value = innInput.value
@@ -40,7 +43,9 @@
             { year: 2016, count: 28 },
         ];
 
-        new Chart(testChart.value,{
+        testChart.value.hidden = false
+
+        new Chart(testChart.value, {
             type: 'bar',
             data: {
                 labels: data.map(row => row.year),
@@ -53,10 +58,19 @@
             }
         })
     }
+
+    function clearFilters(){
+        filters.value = {}
+    }
 </script>
 
 <template>
     <div class="container mt-2 mb-2" v-if="INN != null">
+        <div>
+            filters: {{ filters }}<br>
+            metricsParams: {{ metricsParams }}
+        </div>
+
         <div class="row">
             <!-- Фильтры -->
             <div class="col-2 border">
@@ -65,28 +79,28 @@
                 </div>
                 <div class="mt-2">
                     Код КПГЗ<br>
-                    <input type="text">
+                    <input type="text" v-model="filters.kpgz_code">
                 </div>
                 
                 <div class="mt-2">
                     Дата<br>
-                    Начало: <input type="date">
-                    Конец: <input type="date">
+                    Начало: <input type="date" v-model="filters.dateStart">
+                    Конец: <input type="date" v-model="filters.dateEnd">
                 </div>
                 
                 <div class="mt-2">
                     Регион<br>
-                    <input type="checkbox"> Город 1<br>
-                    <input type="checkbox"> Город 2<br>
-                    <input type="checkbox"> Город 3<br>
+                    <label><input type="checkbox"> Город 1</label><br>
+                    <label><input type="checkbox"> Город 2</label><br>
+                    <label><input type="checkbox"> Город 3</label><br>
                 </div>
 
-                <div>
+                <div class="mt-2">
                     <div>
                         <button @click="showChart">Принять</button>
                     </div>
                     <div>
-                        <button>Очистить</button>
+                        <button @click="clearFilters">Очистить</button>
                     </div>
                 </div>
             </div>
@@ -99,16 +113,19 @@
                 </div>
 
                 <div class="border mt-2">
-                    <div class="text-center">Тут графики</div>
-                    <canvas class="m-2 p-4 border" ref="testChart">
-
-                    </canvas>
+                    <canvas class="m-2 p-4 border" ref="testChart" hidden="true"/>
                 </div>
 
             </div>
 
             <!-- Метрики -->
             <div class="col-2 border">
+                <div class="text-end" data-bs-toggle="collapse" href="#collapseMetricsParams">O</div>
+                <div class="collapse border p-2" id="collapseMetricsParams" ref="metricsParamsCollapseButton">
+                        <label><input type="radio" name="metricsRadioButton" value="param1" v-model="metricsParams.param"> Параметр 1</label>
+                        <label><input type="radio" name="metricsRadioButton" value="param2" v-model="metricsParams.param"> Параметр 2</label>
+                        <label><input type="radio" name="metricsRadioButton" value="param3" v-model="metricsParams.param"> Параметр 3</label>
+                </div>
                 <metric-item v-for="metric in metrics" :metric="metric.metric" :value="metric.value"/>
             </div>
         </div>
