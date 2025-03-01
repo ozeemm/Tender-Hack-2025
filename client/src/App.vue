7848147e7e2d90237168d42a2bd9416cd49ca2c0
ozeemm <77226278+ozeemm@users.noreply.github.com>
2025-03-01 16:32:07 +0800

    Added example chart

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, useTemplateRef, onMounted } from 'vue'
+
+    import Chart from 'chart.js/auto';
 
     import MetricItem from "./components/MetricItem.vue"
     import MyTable from "./components/MyTable.vue"
@@ -17,12 +19,63 @@
             ["01.13.13.01.01", "РАСХОДНЫЕ МАТЕРИАЛЫ И КОМПЛЕКТУЮЩИЕ ДЛЯ ЛАЗЕРНЫХ ПРИНТЕРОВ И МФУ", "7721663977", "2022-12-02 09:18:07.577", "2022-12-02 15:17:08.287" ]
         ]
     })
+
+    const testChart = useTemplateRef('testChart')
+
+    onMounted(() => { 
+        const data = [
+            { year: 2010, count: 10 },
+            { year: 2011, count: 20 },
+            { year: 2012, count: 15 },
+            { year: 2013, count: 25 },
+            { year: 2014, count: 22 },
+            { year: 2015, count: 30 },
+            { year: 2016, count: 28 },
+        ];
+
+        new Chart(testChart.value,{
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.year),
+                datasets: [
+                    {
+                        label: 'Acquisitions by year',
+                        data: data.map(row => row.count)
+                    }
+                ]
+            }
+        })
+    })
 </script>
 
 <template>
-    <div class="container mt-2">
+    <div class="container mt-2 mb-2">
         <div class="row">
-            <div class="col-2 border">Тут фильтры</div>
+            <!-- Фильтры -->
+            <div class="col-2 border">
+                <div class="text-center mt-2">
+                    <h5>Фильтры</h5>
+                </div>
+                <div class="mt-2">
+                    Код КПГЗ<br>
+                    <input type="text">
+                </div>
+                
+                <div class="mt-2">
+                    Дата<br>
+                    Начало: <input type="date">
+                    Конец: <input type="date">
+                </div>
+                
+                <div class="mt-2">
+                    Регион<br>
+                    <input type="checkbox"> Город 1<br>
+                    <input type="checkbox"> Город 2<br>
+                    <input type="checkbox"> Город 3<br>
+                </div>
+            </div>
+
+            <!-- Таблица, графики -->
             <div class="col-8">
 
                 <div class="border">
@@ -31,9 +84,14 @@
 
                 <div class="border mt-2">
                     Тут графики
+                    <canvas ref="testChart">
+
+                    </canvas>
                 </div>
-                
+
             </div>
+
+            <!-- Метрики -->
             <div class="col-2 border">
                 <metric-item v-for="metric in metrics" :metric="metric.metric" :value="metric.value"/>
             </div>
